0c70f9af9fd71b73449ca3dc4736814b846314ce
Yuvraj <yurajsmn@gmail.com>
2026-02-24 13:48:01 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,10 @@
+const dotenv=require('dotenv');
+dotenv.config();
+const express=require('express')
+const cors=require('cors')
+const app=express();
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('Hellow World');
+});
+module.exports=app;
